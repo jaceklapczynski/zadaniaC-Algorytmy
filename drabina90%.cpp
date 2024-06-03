0ecd6253799759d2ad0d8f5609ea7cb65bf835ad
jaceklapczynski <149234980+jaceklapczynski@users.noreply.github.com>
2024-06-03 16:41:44 +0200

    drabina92%.cpp

--- drabina90%.cpp
+++ drabina90%.cpp
@@ -5,33 +5,35 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie (nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int z;
     cin >> z;
 
-    vector<pair<int, int>> data(z);
-    int max_s = 0;
-    int max_p = 0;
+    vector<pair<unsigned int, unsigned int>> data(z);
+    unsigned int max_s = 0;
+    unsigned int max_p = 0;
 
-    for (int i = 0; i < z; ++i) {
-        int s, p;
+    for (unsigned int i = 0; i < z; ++i) {
+        unsigned int s, p;
         cin >> s >> p;
         data[i] = {s, p};
         if (s > max_s) max_s = s;
         if (p > max_p) max_p = p;
     }
 
-    int mod = pow(2, max_p);
-    vector<long long> f(max_s + 1);
+    unsigned int max_mod = 1 << max_p;
+    vector<unsigned long long> f(max_s + 1);
     f[0] = 1;
     f[1] = 1;
 
-    for (int k = 2; k <= max_s; ++k) {
-        f[k] = (f[k - 1] + f[k - 2]) % mod;
+    for (unsigned int k = 2; k <= max_s; ++k) {
+        f[k] = (f[k - 1] + f[k - 2]) % max_mod;
     }
 
     for (const auto& [s, p] : data) {
-        int current_mod = pow(2, p);
+        unsigned int current_mod = 1 << p;
         cout << f[s] % current_mod << endl;
     }
 
